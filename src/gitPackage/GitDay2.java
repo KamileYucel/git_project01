30bc9ddae1db598ccb08563f3a05c7cc69f82aea
KamileYucel <mymac@MyMacs-MacBook-Pro.local>
2020-03-14 16:08:12 -0400

    commit 6 added

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -9,6 +9,7 @@ public class GitDay2 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello Git Users");
+		System.out.println("Check if your local is ahead of origin master");
 		
 		
 	}
